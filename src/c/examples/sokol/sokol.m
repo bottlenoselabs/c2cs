1cc74a17a98d31e07080e376aaede8a61d52f8c6
Lucas Girouard-Stranks <lucas@stranks.ca>
2021-04-29 23:33:33 -0400

    Add sokol; attempt at building using CMake

---
+++ src/c/examples/sokol/sokol.m
@@ -0,0 +1,25 @@
+#define SOKOL_IMPL
+#define SOKOL_DLL
+#define SOKOL_NO_ENTRY
+
+#include <TargetConditionals.h>
+#if TARGET_OS_MAC
+    #include <Cocoa/Cocoa.h>
+#else
+    #error "Not supported Apple platform."    
+#endif
+
+#if SOKOL_METAL
+    #import <Metal/Metal.h>
+#elif SOKOL_GLCORE33
+    #ifndef GL_SILENCE_DEPRECATION
+        #define GL_SILENCE_DEPRECATION
+    #endif
+    #if TARGET_OS_MAC
+        #include <OpenGL/gl3.h>   
+    #endif
+#endif
+
+#include "sokol_app.h"
+#include "sokol_gfx.h"
+#include "sokol_glue.h"
