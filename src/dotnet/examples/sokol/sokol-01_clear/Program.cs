9185dcde0a0659a332970e111c34ff52023891d0
Lucas Girouard-Stranks <lucas@stranks.ca>
2021-05-02 13:13:23 -0400

    Add sokol clear sample

---
+++ src/dotnet/examples/sokol/sokol-01_clear/Program.cs
@@ -0,0 +1,50 @@
+﻿using System.Runtime.InteropServices;
+using lithiumtoast.NativeTools;
+using static sokol;
+
+internal static unsafe class Program
+{
+    private static sg_color _color;
+
+    private static void Main()
+    {
+        var desc = default(sapp_desc);
+        desc.init_cb.Pointer = &Initialize;
+        desc.frame_cb.Pointer = &Frame;
+        desc.cleanup_cb.Pointer = &Cleanup;
+        desc.width = 400;
+        desc.height = 300;
+        desc.gl_force_gles2 = true;
+        desc.window_title = Native.MapCString("Clear (sokol app)");
+        
+        sapp_run(&desc);
+    }   
+    
+    [UnmanagedCallersOnly]
+    public static void Initialize()
+    {
+        var desc = default(sg_desc);
+        desc.context = sapp_sgcontext();
+        sg_setup(&desc);
+    }
+    
+    [UnmanagedCallersOnly]
+    private static void Frame()
+    {
+        var g = _color.g + 0.01f;
+        _color.g = g > 1.0f ? 0.0f : g;
+
+        var passAction = default(sg_pass_action);
+        passAction.colors(0).action = sg_action.SG_ACTION_CLEAR;
+        passAction.colors(0).value = _color;
+        sg_begin_default_pass(&passAction, sapp_width(), sapp_height());
+        sg_end_pass();
+        sg_commit();
+    }
+    
+    [UnmanagedCallersOnly]
+    private static void Cleanup()
+    {
+        sg_shutdown();
+    }
+}
