baba0f15300df63c956f5e761f3c2401a54948d0
Lucas Girouard-Stranks <lucas@stranks.ca>
2021-05-04 23:18:47 -0400

    Flecs example 01

--- src/dotnet/examples/flecs/flecs-01_helloworld/Program.cs
+++ src/dotnet/examples/flecs/flecs-01_helloworld/Program.cs
@@ -1,24 +1,26 @@
-﻿using static flecs;
+﻿using System;
+using System.Runtime.InteropServices;
+using static flecs;
 
 internal static class Program
 {
-    // public unsafe static class Components
-    // {
-    //     public struct PositionComponent
-    //     {
-    //         public double X;
-    //         public double Y;
-    //         
-    //         public static readonly sbyte* Id = NativeTools.MapCString("Position");
-    //         public const ulong Size = 8;
-    //         public const ulong Alignment = 8;
-    //     }
-    // }
-    //
-    // public unsafe static class Entities
-    // {
-    //     public static sbyte* MyEntity = NativeTools.MapCString("MyEntity");
-    // }
+    private static unsafe class Components
+    {
+        public struct PositionComponent
+        {
+            public double X;
+            public double Y;
+            
+            public static readonly sbyte* Name = NativeTools.MapCString("Position");
+            public static ulong Size = (ulong)Marshal.SizeOf<PositionComponent>();
+            public const ulong Alignment = 8;
+        }
+    }
+    
+    public static unsafe class Entities
+    {
+        public static sbyte* MyEntity = NativeTools.MapCString("MyEntity");
+    }
 
     private static unsafe int Main(string[] args)
     {
@@ -27,29 +29,35 @@ internal static class Program
         var argv = NativeTools.MapCStringArray(args);
         var world = ecs_init_w_args(args.Length, (sbyte**) argv);
 
-        // /* Register a component with the world. */
-        // var component = ecs_new_component(world, default, Components.PositionComponent.Id, Components.PositionComponent.Size, Components.PositionComponent.Alignment);
-        //
-        // /* Create a new empty entity  */
-        // var entity = ecs_new_entity(world, default, (sbyte*) 0, (sbyte*) 0);
-        // ecs_set_ptr_w_id(world, Entities.MyEntity, 6,  )
-        //
-        // /* Set the Position component on the entity */
-        // var position = new Components.PositionComponent
-        // {
-        //     X = 10,
-        //     Y = 20
-        // };
-        // var id = new ecs_id_t { Data = component.Data };
-        // ecs_set_ptr_w_id(world, entity, id, Components.PositionComponent.Size, &position);
-        //
-        // /* Get the Position component */
-        // var p = (Components.PositionComponent*) ecs_get_w_id(world, component, id);
-        //
-        // var nameCString = ecs_get_name(world, entity);
-        // var nameString = NativeTools.MapString(nameCString);
-        //
-        // Console.WriteLine($"Position of {nameString} is {p->X}, {p->Y}\n");
+        /* Register a component with the world. */
+        var componentDescriptor = default(ecs_component_desc_t);
+        componentDescriptor.entity.name = Components.PositionComponent.Name;
+        componentDescriptor.size = (ulong) Marshal.SizeOf<Components.PositionComponent>();
+        componentDescriptor.alignment = 8;
+        var component = ecs_component_init(world, &componentDescriptor);
+        var componentId = *(ecs_id_t*)(&component); // TODO: Remove this nasty type cast
+
+        /* Create a new empty entity  */
+        var entityDescriptor = default(ecs_entity_desc_t);
+        entityDescriptor.name = Entities.MyEntity;
+        entityDescriptor.add(0) = componentId; // TODO: Switch to index property to get [] instead of () notation
+        var entity = ecs_entity_init(world, &entityDescriptor);
+
+        /* Set the Position component on the entity */
+        var position = new Components.PositionComponent
+        {
+            X = 10,
+            Y = 20
+        };
+        ecs_set_ptr_w_id(world, entity, componentId, Components.PositionComponent.Size, &position);
+
+        /* Get the Position component */
+        var p = (Components.PositionComponent*) ecs_get_w_id(world, entity, componentId);
+
+        var nameCString = ecs_get_name(world, entity);
+        var nameString = NativeTools.MapString(nameCString);
+        
+        Console.WriteLine($"Position of {nameString} is {p->X}, {p->Y}\n");
 
         /* Cleanup */
         return ecs_fini(world);
