40e430b4d09b8ca7fe669d55143ab86fecc97f2e
Lucas Girouard-Stranks <lustranks@gmail.com>
2021-10-31 19:15:13 -0400

    Remove macros command line verb

--- src/cs/production/C2CS/UseCases/Macros/Response.cs
+++
@@ -1,8 +0,0 @@
-// Copyright (c) Lucas Girouard-Stranks (https://github.com/lithiumtoast). All rights reserved.
-// Licensed under the MIT license. See LICENSE file in the Git repository root directory for full license information.
-
-namespace C2CS.UseCases.Macros;
-
-public class Response : UseCaseResponse
-{
-}
