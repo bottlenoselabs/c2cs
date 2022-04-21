c7faa236df4cd2ee8fbc1e0eb8ce12d50367998b
Lucas Girouard-Stranks <lustranks@gmail.com>
2022-04-20 23:19:08 -0400

    Use XUnit theory instead of fact

--- src/cs/tests/C2CS.Tests.Integration/my_c_library/WriteCodeCSharpTests.cs
+++ src/cs/tests/C2CS.Tests.Integration/my_c_library/WriteCodeCSharpTests.cs
@@ -1,8 +1,6 @@
 // Copyright (c) Bottlenose Labs Inc. (https://github.com/bottlenoselabs). All rights reserved.
 // Licensed under the MIT license. See LICENSE file in the Git repository root directory for full license information.
 
-using System;
-using System.IO;
 using System.IO.Abstractions;
 using System.Runtime.CompilerServices;
 using C2CS.IntegrationTests.my_c_library.Fixtures;
@@ -24,70 +22,32 @@ public class WriteCodeCSharpTests : CLibraryIntegrationTest
     public WriteCodeCSharpTests()
         : base("my_c_library")
     {
-        // Change to `true` to regenerate test files from actual data, then turn to `false` after to use test files.
+        // Change to `true` to regenerate test files from actual data, then turn to `false` after to use the generated test files.
         _regenerateDataFiles = true;
         _fileSystem = TestHost.Services.GetService<IFileSystem>()!;
         _fixture = TestHost.Services.GetService<WriteCodeCSharpFixture>()!;
         _serializer = TestHost.Services.GetService<CSharpGeneratedJsonSerializer>()!;
     }
 
-    [Fact]
-    public void enum_force_uint32()
+    [Theory]
+    [InlineData("enum_force_uint32")]
+    public void Enum(string name)
     {
-        AssertEnum(nameof(enum_force_uint32));
-    }
-
-    [Fact]
-    public void function_void_void()
-    {
-        AssertFunction(nameof(function_void_void));
-    }
-
-    [Fact]
-    public void function_void_string()
-    {
-        AssertFunction(nameof(function_void_string));
-    }
-
-    [Fact]
-    public void function_void_uint16_int32_uint64()
-    {
-        AssertFunction(nameof(function_void_uint16_int32_uint64));
-    }
-
-    [Fact]
-    public void function_void_uint16ptr_int32ptr_uint64ptr()
-    {
-        AssertFunction(nameof(function_void_uint16ptr_int32ptr_uint64ptr));
-    }
-
-    [Fact]
-    public void function_void_enum()
-    {
-        AssertFunction(nameof(function_void_enum));
-    }
-
-    [Fact]
-    public void function_void_struct_union_anonymous()
-    {
-        AssertFunction(nameof(function_void_struct_union_anonymous));
-    }
-
-    [Fact]
-    public void function_void_struct_union_named()
-    {
-        AssertFunction(nameof(function_void_struct_union_named));
-    }
-
-    private void AssertFunction(string name)
-    {
-        var value = _fixture.GetFunction(name);
+        var value = _fixture.GetEnum(name);
         AssertValue(name, value);
     }
 
-    private void AssertEnum(string name)
+    [Theory]
+    [InlineData("function_void_void")]
+    [InlineData("function_void_string")]
+    [InlineData("function_void_uint16_int32_uint64")]
+    [InlineData("function_void_uint16ptr_int32ptr_uint64ptr")]
+    [InlineData("function_void_enum")]
+    [InlineData("function_void_struct_union_anonymous")]
+    [InlineData("function_void_struct_union_named")]
+    public void Function(string name)
     {
-        var value = _fixture.GetEnum(name);
+        var value = _fixture.GetFunction(name);
         AssertValue(name, value);
     }
 
@@ -106,7 +66,7 @@ public class WriteCodeCSharpTests : CLibraryIntegrationTest
     private void RegenerateDataFile<T>(string name, T value, [CallerFilePath] string? sourceCodeFilePath = null)
     {
         var directory = _fileSystem.Path.GetDirectoryName(sourceCodeFilePath);
-        var jsonFilePath = _fileSystem.Path.Combine(directory, "Data", $"{name}.json");
+        var jsonFilePath = _fileSystem.Path.Combine(directory, "Data", name, $"{name}.json");
         _serializer.WriteToFile(jsonFilePath, value);
     }
 }
