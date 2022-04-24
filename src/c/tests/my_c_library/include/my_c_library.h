f041baf90a5620df317d21a3a27e9652335fd49e
Lucas Girouard-Stranks <lustranks@gmail.com>
2022-04-23 21:22:16 -0400

    Added tests for #72 (#81)
    * Added tests for #72
    
    * Add test files for Windows and Linux
    
    * Comment out sniping test files

--- src/c/tests/my_c_library/include/my_c_library.h
+++ src/c/tests/my_c_library/include/my_c_library.h
@@ -61,10 +61,48 @@ typedef struct struct_union_named
 } struct_union_named;
 
 PINVOKE_API_DECL void function_void_void(void);
+PINVOKE_API_DECL void function_void_int(int);
+
+PINVOKE_API_DECL void function_void_intptr(int*);
+PINVOKE_API_DECL void function_void_intptr_1(int* );
+PINVOKE_API_DECL void function_void_intptr_2(int *);
+PINVOKE_API_DECL void function_void_intptr_3(int*ptr);
+PINVOKE_API_DECL void function_void_intptr_4(int* ptr);
+PINVOKE_API_DECL void function_void_intptr_5(int *ptr);
+PINVOKE_API_DECL void function_void_intptr_6(int * ptr);
+PINVOKE_API_DECL void function_void_intptr_7(const int*);
+PINVOKE_API_DECL void function_void_intptr_8(const int* );
+PINVOKE_API_DECL void function_void_intptr_9(const int *);
+PINVOKE_API_DECL void function_void_intptr_10(const int*ptr);
+PINVOKE_API_DECL void function_void_intptr_11(const int* ptr);
+PINVOKE_API_DECL void function_void_intptr_12(const int *ptr);
+PINVOKE_API_DECL void function_void_intptr_13(const int * ptr);
+PINVOKE_API_DECL void function_void_intptr_14(int const*);
+PINVOKE_API_DECL void function_void_intptr_15(int const* );
+PINVOKE_API_DECL void function_void_intptr_16(int const *);
+PINVOKE_API_DECL void function_void_intptr_17(int const*ptr);
+PINVOKE_API_DECL void function_void_intptr_18(int const* ptr);
+PINVOKE_API_DECL void function_void_intptr_19(int const *ptr);
+PINVOKE_API_DECL void function_void_intptr_20(int const * ptr);
+PINVOKE_API_DECL void function_void_intptr_21(int const* const);
+PINVOKE_API_DECL void function_void_intptr_22(int const*const);
+PINVOKE_API_DECL void function_void_intptr_23(int const *const);
+PINVOKE_API_DECL void function_void_intptr_24(int const * const);
+PINVOKE_API_DECL void function_void_intptr_25(int const* const ptr);
+PINVOKE_API_DECL void function_void_intptr_26(int const*const ptr);
+PINVOKE_API_DECL void function_void_intptr_27(int const *const ptr);
+PINVOKE_API_DECL void function_void_intptr_28(int const * const ptr);
+
 PINVOKE_API_DECL void function_void_string(const char* s);
+
 PINVOKE_API_DECL void function_void_uint16_int32_uint64(uint16_t a, int32_t b, uint64_t c);
+
 PINVOKE_API_DECL void function_void_uint16ptr_int32ptr_uint64ptr(const uint16_t* a, const int32_t* b, const uint64_t* c);
+
 PINVOKE_API_DECL void function_void_enum(const enum_force_uint32 e);
+
 PINVOKE_API_DECL void function_void_struct_union_anonymous(const struct_union_anonymous s);
+
 PINVOKE_API_DECL void function_void_struct_union_anonymous_with_field_name(const struct_union_anonymous_with_field_name s);
+
 PINVOKE_API_DECL void function_void_struct_union_named(const struct_union_named s);
