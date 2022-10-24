42185e69480902d5970ffb3c51d95d0fd6a9302b
Lucas Girouard-Stranks <lithiumtoast@users.noreply.github.com>
2022-10-24 08:19:23 -0400

    Move files around to remove `infrastructure` project folder

--- src/cs/production/C2CS.Plugin/Foundation/UseCases/Exceptions/UseCaseStepFailedException.cs
+++ src/cs/production/C2CS.Plugin/Foundation/UseCases/Exceptions/UseCaseStepFailedException.cs
@@ -1,8 +1,6 @@
 // Copyright (c) Bottlenose Labs Inc. (https://github.com/bottlenoselabs). All rights reserved.
 // Licensed under the MIT license. See LICENSE file in the Git repository root directory for full license information.
 
-using System;
-
 namespace C2CS.Foundation.UseCases.Exceptions;
 
 #pragma warning disable CA1032
