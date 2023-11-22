9188e6d2af044ff2fb5c12ef82703bb54bb51349
Lucas Girouard-Stranks <519592+lithiumtoast@users.noreply.github.com>
2023-11-21 22:05:50 -0500

    Update test c library build script

--- src/c/tests/_container_library/build.sh
+++ src/c/tests/_container_library/build.sh
@@ -1,7 +1,8 @@
 #!/bin/bash
 DIRECTORY="$( cd "$( dirname "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )"
+OUPUT_DIRECTORY=$DIRECTORY/bin
 
-rm -rf ./cmake-build-release
-cmake -S . -B cmake-build-release -DCMAKE_BUILD_TYPE=Release -DCMAKE_ARCHIVE_OUTPUT_DIRECTORY=/Users/lstranks/Programming/bottlenoselabs/c2cs/bin/C2CS.Tests/Debug/net7.0/c/tests/_container_library/bin -DCMAKE_LIBRARY_OUTPUT_DIRECTORY=/Users/lstranks/Programming/bottlenoselabs/c2cs/bin/C2CS.Tests/Debug/net7.0/c/tests/_container_library/bin -DCMAKE_RUNTIME_OUTPUT_DIRECTORY=/Users/lstranks/Programming/bottlenoselabs/c2cs/bin/C2CS.Tests/Debug/net7.0/c/tests/_container_library/bin -DCMAKE_RUNTIME_OUTPUT_DIRECTORY_RELEASE=/Users/lstranks/Programming/bottlenoselabs/c2cs/bin/C2CS.Tests/Debug/net7.0/c/tests/_container_library/bin
+rm -rf $DIRECTORY/cmake-build-release
+cmake -S $DIRECTORY -B cmake-build-release -DCMAKE_BUILD_TYPE=Release -DCMAKE_ARCHIVE_OUTPUT_DIRECTORY=$OUPUT_DIRECTORY -DCMAKE_RUNTIME_OUTPUT_DIRECTORY=$OUPUT_DIRECTORY -DCMAKE_RUNTIME_OUTPUT_DIRECTORY_RELEASE=$OUPUT_DIRECTORY
 cmake --build cmake-build-release --config Release
-rm -rf ./cmake-build-release
+rm -rf $DIRECTORY/cmake-build-release
