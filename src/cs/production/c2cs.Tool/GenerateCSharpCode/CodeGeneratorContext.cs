c106b4c202816980d4dcb4af7106a0afa20d6017
Lucas Girouard-Stranks <519592+lithiumtoast@users.noreply.github.com>
2025-01-07 19:15:44 -0500

    Global using

--- src/cs/production/c2cs.Tool/GenerateCSharpCode/CodeGeneratorContext.cs
+++ src/cs/production/c2cs.Tool/GenerateCSharpCode/CodeGeneratorContext.cs
@@ -1,7 +1,6 @@
 // Copyright (c) Bottlenose Labs Inc. (https://github.com/bottlenoselabs). All rights reserved.
 // Licensed under the MIT license. See LICENSE file in the Git repository root directory for full license information.
 
-using System;
 using System.Collections.Generic;
 using System.Collections.Immutable;
 using bottlenoselabs.Common.Tools;
