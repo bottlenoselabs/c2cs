beab155a848ff6919d7f5dc33e5e9c99b8e08715
Ole Rosskamp <ole.rosskamp@rosslight.de>
2025-04-19 09:58:31 +0200

    Reformatting xml docs

--- src/cs/production/Interop.Runtime/AllocatorExtensions.cs
+++ src/cs/production/Interop.Runtime/AllocatorExtensions.cs
@@ -6,7 +6,7 @@ using System;
 namespace Interop.Runtime;
 
 /// <summary>
-///     Provides extension methods for allocating memory using the <see cref="INativeAllocator"/> interface.
+///     Provides extension methods for allocating memory using the <see cref="INativeAllocator" /> interface.
 /// </summary>
 public static unsafe class AllocatorExtensions
 {
@@ -15,7 +15,7 @@ public static unsafe class AllocatorExtensions
     /// </summary>
     /// <param name="allocator">The allocator used to allocate the memory.</param>
     /// <typeparam name="T">The unmanaged type to allocate memory for.</typeparam>
-    /// <returns>A pointer to the allocated memory for the instance of type <typeparamref name="T"/>.</returns>
+    /// <returns>A pointer to the allocated memory for the instance of type <typeparamref name="T" />.</returns>
     public static T* AllocateSingle<T>(this INativeAllocator allocator)
         where T : unmanaged
     {
@@ -24,13 +24,13 @@ public static unsafe class AllocatorExtensions
     }
 
     /// <summary>
-    ///     Allocates a contiguous block of memory for a specified number of elements of type <typeparamref name="T"/>
+    ///     Allocates a contiguous block of memory for a specified number of elements of type <typeparamref name="T" />
     ///     and returns it as a pointer to the first element.
     /// </summary>
     /// <param name="allocator">The allocator used to allocate the memory.</param>
     /// <param name="elementCount">The number of elements in the array.</param>
     /// <typeparam name="T">The unmanaged type of the elements to allocate memory for.</typeparam>
-    /// <returns>A pointer of type <typeparamref name="T"/> to the first element in the allocated array.</returns>
+    /// <returns>A pointer of type <typeparamref name="T" /> to the first element in the allocated array.</returns>
     public static T* AllocateArray<T>(this INativeAllocator allocator, int elementCount)
         where T : unmanaged
     {
@@ -39,13 +39,13 @@ public static unsafe class AllocatorExtensions
     }
 
     /// <summary>
-    ///     Allocates a contiguous block of memory for a specified number of elements of type <typeparamref name="T"/>
-    ///     and returns it as a <see cref="Span{T}"/>.
+    ///     Allocates a contiguous block of memory for a specified number of elements of type <typeparamref name="T" />
+    ///     and returns it as a <see cref="Span{T}" />.
     /// </summary>
     /// <param name="allocator">The allocator used to allocate the memory.</param>
     /// <param name="elementCount">The number of elements to allocate memory for.</param>
     /// <typeparam name="T">The unmanaged type of the elements to allocate memory for.</typeparam>
-    /// <returns>A <see cref="Span{T}"/> representing the allocated memory block.</returns>
+    /// <returns>A <see cref="Span{T}" /> representing the allocated memory block.</returns>
     public static Span<T> AllocateSpan<T>(this INativeAllocator allocator, int elementCount)
         where T : unmanaged
     {
@@ -54,11 +54,11 @@ public static unsafe class AllocatorExtensions
     }
 
     /// <summary>
-    ///     Allocates memory for a C-style string based on the provided .NET string and returns a <see cref="CString"/>.
+    ///     Allocates memory for a C-style string based on the provided .NET string and returns a <see cref="CString" />.
     /// </summary>
     /// <param name="allocator">The allocator used to allocate the memory.</param>
     /// <param name="str"> The UTF-16 encoded .NET string to be converted into a null-terminated C string. </param>
-    /// <returns> A <see cref="CString"/> representing the allocated C-style string. </returns>
+    /// <returns> A <see cref="CString" /> representing the allocated C-style string. </returns>
     public static CString AllocateCString(this INativeAllocator allocator, string str)
     {
         return CString.FromString(allocator, str);
