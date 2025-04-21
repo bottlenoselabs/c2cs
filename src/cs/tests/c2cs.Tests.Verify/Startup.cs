5b1e3594cc85979b1760d629aae7133c1aab279a
Ole Rosskamp <ole.rosskamp@rosslight.de>
2025-04-21 11:37:04 +0200

    Add better Verify error debugging experience

--- src/cs/tests/c2cs.Tests.Verify/Startup.cs
+++ src/cs/tests/c2cs.Tests.Verify/Startup.cs
@@ -1,9 +1,11 @@
 // Copyright (c) Bottlenose Labs Inc. (https://github.com/bottlenoselabs). All rights reserved.
 // Licensed under the MIT license. See LICENSE file in the Git repository root directory for full license information.
 
+using System.Runtime.CompilerServices;
 using C2CS.BuildCLibrary;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
+using VerifyTests.DiffPlex;
 using Xunit.DependencyInjection.Logging;
 
 namespace C2CS.Tests.Verify;
@@ -12,6 +14,17 @@ namespace C2CS.Tests.Verify;
 // See https://github.com/pengweiqhca/Xunit.DependencyInjection?tab=readme-ov-file#how-to-find-startup
 public sealed class Startup
 {
+    [ModuleInitializer]
+    public static void Initialize() => VerifyDiffPlex.Initialize(OutputType.Compact);
+
+    [ModuleInitializer]
+    public static void OtherInitialize()
+    {
+        VerifierSettings.InitializePlugins();
+        VerifierSettings.ScrubLinesContaining("DiffEngineTray");
+        VerifierSettings.IgnoreStackTrace();
+    }
+
     public void ConfigureHost(IHostBuilder hostBuilder) => hostBuilder.BuildHostCommon();
 
     public void ConfigureServices(IServiceCollection services) => services
